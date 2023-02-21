a62d7d6f7974e5e6787316f0c0c4b2e51f295156
Ananda250394 <118291425+Ananda250394@users.noreply.github.com>
2023-02-21 07:36:48 +0530

    changed a to 6

--- Addition.java
+++ Addition.java
@@ -3,7 +3,7 @@ class Addition{
     public static void main(String[] args) {
     int x = 5; 
     int y = 4;
-    int a = 2;
+    int a = 8;
     int z = x + y + a;
     System.out.println(z);
     }
